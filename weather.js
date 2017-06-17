f5713091e13e116bc8323f9499288c3da2afefbc
Dani <danielle.volker@gmail.com>
2017-06-17 15:36:36 -0400

    api request works (not in codepen?). need to fill out rest of information and style

--- weather.js
+++ weather.js
@@ -1,35 +1,34 @@
 $(document).ready(function(){
 
-  //set variables for long & lat to plug into url variable
-  var longitude;
-  var latitude;
+  //set variables for url
+  var url ="";
 
   //pull location
-  if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-    longitude = position.coords.longitude;
-    latitude = position.coords.latitude;
-  });
-}
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        //$("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
 
-  //variable to write url ending for lat & lon returned from geolocation
-  var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude +"?APPID=647139742b6f938ad3a17203330d6ea3";
+        url = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude +"&APPID=647139742b6f938ad3a17203330d6ea3";
 
-  console.log(url);
-  //make API request for weather at location from openweathermap
-  $.getJSON(url, function(weather){
+        console.log(url);
 
+        //make API request for weather at location from openweathermap
+        $.getJSON(url, function(weather){
 
-  //switch statments to assign #skies based on result of weather.main
+        //switch statments to assign #skies based on result of weather.main
 
-  //convert main.temp to Fahrenheit and assign o #temp
-  $("#temp").html(main.temp * 9 / 5 - 459.67);
-  //assign name to #location
-  $("#location").html(name);
-  //if, else if, else statements to assign classes that match weather
+        //convert main.temp to Fahrenheit and assign o #temp
+        $("#temp").html(weather.main.temp * 9 / 5 - 459.67);
 
-  });
+        //assign name to #location
+        $("#location").html(weather.name);
 
-  //toggle button: Fahrenheit to Celsius using formula
+        //if, else if, else statements to assign classes that match weather
+
+        //toggle button: Fahrenheit to Celsius using formula
+
+    });
+
+      });
+    }
 });
