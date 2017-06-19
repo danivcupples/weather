739f7979762b1d1de2e5861f7ec0b6b5f60ec364
Dani <danielle.volker@gmail.com>
2017-06-19 16:00:05 -0400

    full functionality. all freecodecamp user stories fulfilled

--- weather.js
+++ weather.js
@@ -8,25 +8,40 @@ $(document).ready(function(){
       navigator.geolocation.getCurrentPosition(function(position) {
         //$("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
 
-        url = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude +"&APPID=647139742b6f938ad3a17203330d6ea3";
+        url = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude +"&units=imperial&APPID=647139742b6f938ad3a17203330d6ea3";
 
         console.log(url);
 
         //make API request for weather at location from openweathermap
-        $.getJSON(url, function(weather){
+        $.getJSON(url, function(currentWeather){
 
-        //switch statments to assign #skies based on result of weather.main
+        //assign icon to #weather-icon
+        var iconUrl = "http://openweathermap.org/img/w/" + currentWeather.weather[0].icon + ".png";
+        console.log(iconUrl);
+
+        $("#weather-icon").html("<img alt='current weather icon' class='img-responsive' src='"+ iconUrl + "'>")
+
+        //assign #skies based on result of weather.main & weather.description
+        $("#skies").html(currentWeather.weather[0].description);
 
         //convert main.temp to Fahrenheit and assign o #temp
-        $("#temp").html(weather.main.temp * 9 / 5 - 459.67);
+        $("#temp").html(Math.round((currentWeather.main.temp ) * 10)/10 + "&#176");
+
+        //assign #tempCelsius
+        $("#tempCelsius").html(Math.round(((currentWeather.main.temp - 32) * 5 / 9)*10)/10 + "&#176");
+
+        //assign #humidity
+        $("#humidity").html(currentWeather.main.humidity);
 
         //assign name to #location
-        $("#location").html(weather.name);
+        $("#location").html(currentWeather.name);
 
         //if, else if, else statements to assign classes that match weather
 
         //toggle button: Fahrenheit to Celsius using formula
-
+        $("button").click(function(){
+          $(".temperature p").toggle();
+        })
     });
 
       });
