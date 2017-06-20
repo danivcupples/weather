defa658e5be8f478a4aa1942fefd623c04e76d3c
Dani <danielle.volker@gmail.com>
2017-06-20 16:05:36 -0400

    Using openweathermap api. full functionality in locally hosted files, not on codepen.

--- weather.js
+++ weather.js
@@ -4,41 +4,37 @@ $(document).ready(function(){
   var url ="";
 
   //pull location
-  if (navigator.geolocation) {
-       navigator.geolocation.getCurrentPosition(function(position) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        //$("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
 
-         $.ajax({
-           dataType: "jsonp",
-           url: "https://api.darksky.net/forecast/62f9dc2eac7a8f315e007e9fb0f3c3ae/" + position.coords.latitude +","+ position.coords.longitude,
-           data: currentWeather,
-         });
-        //url = "https://api.darksky.net/forecast/62f9dc2eac7a8f315e007e9fb0f3c3ae/" + position.coords.latitude +","+ position.coords.longitude;
+        url = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude +"&units=imperial&APPID=647139742b6f938ad3a17203330d6ea3";
 
-        //console.log(url);
+        console.log(url);
 
         //make API request for weather at location from openweathermap
-        //$.getJSON(url, function(currentWeather){
+        $.getJSON(url, function(currentWeather){
 
         //assign icon to #weather-icon
-        //var iconUrl = "http://openweathermap.org/img/w/" + currentWeather.weather[0].icon + ".png";
-        //console.log(iconUrl);
+        var iconUrl = "http://openweathermap.org/img/w/" + currentWeather.weather[0].icon + ".png";
+        console.log(iconUrl);
 
         $("#weather-icon").html("<img alt='current weather icon' class='img-responsive' src='"+ iconUrl + "'>")
 
         //assign #skies based on result of weather.main & weather.description
-        $("#skies").html(currentWeather.currently.summary);
+        $("#skies").html(currentWeather.weather[0].description);
 
         //convert main.temp to Fahrenheit and assign o #temp
-        $("#temp").html(Math.round((currentWeather.currently.temperature ) * 10)/10 + "&#176");
+        $("#temp").html(Math.round((currentWeather.main.temp ) * 10)/10 + "&#176");
 
         //assign #tempCelsius
-        $("#tempCelsius").html(Math.round(((currentWeather.currently.temperature - 32) * 5 / 9)*10)/10 + "&#176");
+        $("#tempCelsius").html(Math.round(((currentWeather.main.temp - 32) * 5 / 9)*10)/10 + "&#176");
 
         //assign #humidity
-        $("#humidity").html(currentWeather.currently.humidity);
+        $("#humidity").html(currentWeather.main.humidity);
 
         //assign name to #location
-        $("#location").html(position.city);
+        $("#location").html(currentWeather.name);
 
         //if, else if, else statements to assign classes that match weather
 
@@ -46,7 +42,7 @@ $(document).ready(function(){
         $("button").click(function(){
           $(".temperature p").toggle();
         })
-    //});
+    });
 
       });
     }
